89e4a99672476781792fd55e645c6b23fa7218ef
DSumm18 <167764656+DSumm18@users.noreply.github.com>
2025-03-22 11:14:52 +0000

    Add providers file to app directory to fix build error

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,21 @@
-'use client';
+'use client'
 
-import { SessionProvider } from 'next-auth/react';
-import { Toaster } from 'sonner';
+import { ThemeProvider } from 'next-themes'
+import { SessionProvider } from 'next-auth/react'
+import { Toaster } from 'sonner'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <Toaster position="top-right" />
-      {children}
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Toaster position="top-right" />
+        {children}
+      </ThemeProvider>
     </SessionProvider>
-  );
+  )
 }
